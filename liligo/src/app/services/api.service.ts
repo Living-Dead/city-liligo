ce81fd4dadda8a37e003ad8380f22a83dbf8367a
Living-Dead <horvth.mikls@gmail.com>
2019-04-23 16:10:17 +0200

    FIX | api service

--- liligo/src/app/services/api.service.ts
+++ liligo/src/app/services/api.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+	constructor(
+		private http: HttpClient,
+	) { }
+
+	city(data: any) {
+		return this.http.post(environment.apiUrl + '/citysearch', { city: data });
+	}
 }

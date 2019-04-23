1e5b464d878e3d025a47dd2a0679011c3d9dac88
Living-Dead <horvth.mikls@gmail.com>
2019-04-23 16:48:36 +0200

    FIX | typehead style

--- liligo/src/app/typehead/typehead.component.ts
+++ liligo/src/app/typehead/typehead.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
 import { Observable } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
 
 @Component({
 	selector: 'app-typehead',
@@ -12,8 +13,7 @@ export class TypeheadComponent {
 
 	private cityList: any;
 	private selectCity: string;
-	private text: Object;
-	private searchValue: string = '';
+	private text: object;
 
 	constructor(
 		private apiService: ApiService,
@@ -23,16 +23,15 @@ export class TypeheadComponent {
 		this.text = {
 			label: 'City name',
 			button: 'Search city on the GOOGLE',
-
 		}
 	}
 
 	onKey(event: any) {
+		this.selectCity = '';
 		if (event.target.value.length >= 1) {
 			this.apiService
 				.city(event.target.value)
 				.subscribe(cityList => {
-					console.log('state', cityList);
 					this.cityList = cityList !== '' ? cityList : [];
 				})
 		} else {
@@ -41,16 +40,13 @@ export class TypeheadComponent {
 	}
 
 	selectFromSuggestions(data) {
+		this.cityList.city = [];
+		this.cityList.city.push(data);
 		this.selectCity = data;
 	}
 
-	clearSearch() {
-		this.searchValue = ' ';
-	}
-
 	goToGoogle(data) {
-		window.open('https://www.google.com/search?q=' + data + '&tbm=isch', '_blank');
-		this.cityList = [];
-		this.clearSearch();
+		window.open(environment.google + '/search?q=' + data + '&tbm=isch', '_blank');
+		this.cityList.city = [];
 	}
 }

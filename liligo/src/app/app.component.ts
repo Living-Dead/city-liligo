b78c688856f52685b0e6f03a78730228ef64dca9
Living-Dead <horvth.mikls@gmail.com>
2019-04-23 16:13:34 +0200

    FIX | app component scss ts html

--- liligo/src/app/app.component.ts
+++ liligo/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'liligo';
+	title = 'liligo';
+	appName = 'City Search App';
 }
